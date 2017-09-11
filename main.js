84e456bfddcd239f2e349e1a7d22692466320c12
Seth J. Moore <seth@hsimosoftware.com>
2017-09-11 19:50:13 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,135 @@
+const h = snabbdom.h;
+patch = snabbdom.init([snabbdom_attributes.attributesModule]);
+
+function componentOne(state) {
+
+  state({player: {x: 0, y: 0}, puppy: {x: 50, y: 50}});
+
+  const keypresses = flyd.stream();
+  const keypressActions = flyd.stream();
+  flyd.on(evnt => {
+    let action;
+    switch (evnt.code) {
+      case "KeyJ":
+        action = {action: 'SOUTH'};
+        break;
+      case "KeyK":
+        action = {action: 'NORTH'};
+        break;
+      case "KeyH":
+        action = {action: 'WEST'};
+        break;
+      case "KeyL":
+        action = {action: 'EAST'};
+        break;
+      default:
+        action = undefined;
+    }
+    if (action !== undefined) {
+      keypressActions(action);
+    }
+  }, keypresses)  
+  
+  const actions = keypressActions;
+
+  function updateState(oldState, action) {
+    let newState;
+    switch (action.action) {
+      case 'SOUTH':
+        newState = {
+          player: {
+            x: oldState.player.x,
+            y: oldState.player.y + 1
+          },
+          puppy: oldState.puppy
+        };
+        break;
+      case 'NORTH':
+        newState = {
+          player: {
+            x: oldState.player.x,
+            y: oldState.player.y - 1
+          },
+          puppy: oldState.puppy
+        };
+        break;
+      case 'WEST':
+        newState = {
+          player: {
+            x: oldState.player.x - 1,
+            y: oldState.player.y
+          },
+          puppy: oldState.puppy
+        };
+        break;
+      case 'EAST':
+        newState = {
+          player: {
+            x: oldState.player.x + 1,
+            y: oldState.player.y
+          },
+          puppy: oldState.puppy
+        };
+        break;
+      default:
+        newState = oldState;
+    }
+    if (Math.abs(newState.player.x - newState.puppy.x) < 5 && 
+        Math.abs(newState.player.y - newState.puppy.y) < 5 ) {
+      newState.win = true;
+    }
+    return newState;
+  }
+
+  flyd.on(action => state(updateState(state(), action)), actions);
+
+  const vdom = flyd.map(newState => createVDom(newState), state);
+
+  function createVDom(localState) {
+    return h('div.field', [
+      localState.win ? h('h1', 'You won!!!') :
+      h('svg', {attrs: {width: '100%', height: '100%'}}, [
+       createPlayerSVG(localState.player),
+       createPuppySVG(localState.puppy),
+      ])
+    ]);
+  }
+  
+  function createPlayerSVG(player) {
+    return h('circle', {attrs: {
+      cx: player.x,
+      cy: player.y,
+      r: 5,
+      stroke: 'green',
+      'stroke-width': 2,
+      fill: 'red'
+    }})
+  }
+  
+  function createPuppySVG(puppy) {
+    return h('circle', {attrs: {
+      cx: puppy.x,
+      cy: puppy.y,
+      r: 5,
+      stroke: 'blue',
+      'stroke-width': 2,
+      fill: 'purple'
+    }})
+  }
+
+  const setup = () => {
+    document.body.addEventListener('keypress', keypresses);
+  };
+  
+  return {DOM: vdom, setup: setup};
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  const state = flyd.stream({entry: flyd.stream()});
+  const entry = componentOne(state().entry);
+  const vdoms = flyd.scan((acc, newVDOM) => [acc[1], newVDOM],
+                          [null, document.getElementById('app')],
+                          entry.DOM);
+  flyd.on(([oldV, newV]) => patch(oldV, newV), vdoms);
+  entry.setup();
+});
